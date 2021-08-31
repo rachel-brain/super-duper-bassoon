289f4a3544afd0bd9952a14a4a21ed0a1cad34f7
engn.mustafa <engn.mustafa@yahoo.com>
2021-08-31 13:29:09 +1000

    created the base for the databse and log in/log out

--- models/index.js
+++ models/index.js
@@ -1,15 +1,37 @@
-// THIS CODE WILL NEED CHECKING & MODIFYING!
+'use strict';
 
-const Player = require('./Player');
-const Something = require('./Something');
-const Anotherthing = require('./Anotherthing');
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
 
-Something.hasMany(Anotherthing, {
-  foreignKey: 'something_id',
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
-Anotherthing.belongsTo(Something, {
-  foreignKey: 'something_id',
-});
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-module.exports = { Player, Something, Anotherthing };
+module.exports = db;
