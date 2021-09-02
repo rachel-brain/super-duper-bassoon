91eff8f18c353bd9d3c3d45c1c4b631c0c8d654f
engn.mustafa <engn.mustafa@yahoo.com>
2021-09-02 20:21:05 +1000

    adding the files again

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
-const User = require('./User');
-const Stat = require('./stat');
+// const User = require('./User');
+// const Stat = require('./stat');
 
-User.hasMany(Stat, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
+// User.hasMany(Stat, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
 
-Stat.belongsTo(User, {
-  foreignKey: 'user_id'
-});
+// Stat.belongsTo(User, {
+//   foreignKey: 'user_id'
+// });
 
-module.exports = { User, Stat };
+// module.exports = { User, Stat };
