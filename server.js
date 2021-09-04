78d9c4e76a723916333cc5b77d6d69a14b420e81
Sooova <83949034+Sooova@users.noreply.github.com>
2021-09-04 23:50:00 +1000

    more login changes

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 require('dotenv').config()
 const sequelize = require('./config/connection');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+var bodyParser = require('body-parser');
 //require and start express and handlebars
 const express = require('express');
 const app = express();
 var exphbs = require('express-handlebars');
 const path = require('path');
+
+
+//middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}))
 
 //routes folder
 const routes = require('./controllers');
@@ -23,13 +33,6 @@ const models = require('./models');
 const {
     addHook
 } = require('./models/user');
-
-
-//middleware for parsing JSON and urlencoded form data
-app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}))
 
 //enable the environment to specify a port, or use the default port. BC heruoku will generate a random port and use that.
 const PORT = process.env.PORT || 8080;
@@ -83,3 +86,16 @@ db.sync().then(() => {
     app.listen(PORT);
     console.log('listening on port 8080')
 });
+
+//session
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: db
+    })
+  };
+
+  app.use(session(sess));
